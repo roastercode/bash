6252281fb89dc007f58a7f061bbe44ae3eeeb718
Aurelien DESBRIERES <aurelien.desbrieres.ext@zodiacaerospace.com>
2018-01-31 10:14:22 +0100

    Deeper in Bash & Algorithms

--- square.bash
+++ square.bash
@@ -3,34 +3,8 @@
 # Contact: aurelien@hackers.camp
 # License: GNU GPL v3 or later
 
-# Perimeter calculus with its algorithm
+# Square draw & Perimeter calculus with its algorithm
 
-# VAR       :  L, l
-# PERIMETER :  L*l
-# RESULT    :  print calculus result
-
-# # # L=3
-# # # l=2
-# # # p=$((L*l))
-# # # printf "Perimeter is: %s\n" "$p"
-
-
-# REQUEST & SAVE : L
-# SAVE L AS A DATA
-# REQUEST : l
-# SAVE L AS A DATA
-# USE L AND l TO CALCULATE PERIMETER
-# PRINT PERIMETER
-
-# # # echo "what is L"
-# # # read L
-# # # echo "what is l"
-# # # read l
-# # # p=$((L*l))
-# # # printf "Perimeter is: %s\n" "$p"
-
-
-# PERIMETER REQUEST, DRAW & CALCULUS
 # REQUEST : L
 # SAVE L AS A DATA
 # REQUEST : l
